fa0beaca718e927a114605a95b97166b9afcaed5
Nick Rabone <n.rabone72@aol.com>
2024-07-14 12:43:14 +0100

    Refactored and componentised further

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
 import './style.css'
+import App from './App'
 
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
-    </mesh>
-  )
-}
-
-function App() {
-  return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box />
-    </Canvas>
-  )
-}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
